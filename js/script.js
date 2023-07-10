678ddca76386b5d77042d130b42bc6ea2a156e37
hanslaptop <hramoscampos@gmail.com>
2023-07-10 18:52:31 -0500

    commit 7

--- js/script.js
+++ js/script.js
@@ -5,7 +5,14 @@ const addNewTask = event => {
     const { value } = event.target.taskText;
     if (!value) return;
     const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder')
+    task.addEventListener('click', changeTaskState)
     task.textContent = value;
     tasksContainer.prepend(task);
     event.target.reset();
 }
+
+const changeTaskState  = event =>{
+    event.target.classList.toggle('done');
+}
+
